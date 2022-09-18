a021d9c627515bde366ca21f24742555280a2427
r0ck1n70sh <r0ck1n70sh@gmail.com>
2022-09-19 00:35:44 +0530

    testing -- 101

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -3,7 +3,19 @@ pragma solidity ^0.8.17;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint256 totalWaves;
+    
     constructor() {
         console.log("hello! i'm a contract");
+    }
+
+    function wave() public {
+        totalWaves += 1;
+        console.log("%s has waved!", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("we've %d total waves", totalWaves);
+        return totalWaves;
     }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,9 +1,19 @@
 const main = async () => {
+    const [owner, randomPersons] = await hre.ethers.getSigners();
+
     const waveContractFactory = await hre.ethers.getContractFactory("WavePortal")
     const waveContract = await waveContractFactory.deploy()
     await waveContract.deployed()
 
-    console.log("@r0ck1n70sh Contract deployed to: ", waveContract.address);
+    console.log("Contract deployed to: ", waveContract.address);
+    console.log("Contract deployed by: ", owner.address);
+
+    let waveCount = await waveContract.getTotalWaves();
+
+    let waveTxn = await waveContract.wave(); 
+    waveTxn.wait();
+
+    waveCount = await waveContract.getTotalWaves()
 }
 
 const runMain = async () => {
